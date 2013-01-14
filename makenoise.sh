a88cd0e579212959ffe07e2fe5668130bff215e4
Ralf Karrenberg <karrenberg@cdl.uni-saarland.de>
2013-01-14 14:53:25 +0100

    Added sample build script that sets up required variables.

---
+++ makenoise.sh
@@ -0,0 +1,5 @@
+export LLVM_INSTALL_DIR=/home/karrenberg/proj/noise/install_debug
+export WFV_INSTALL_DIR=/home/karrenberg/proj/wfv2/build/linux-debug/
+
+# Build Clang.
+make clang-only -C ../../../build_debug/
