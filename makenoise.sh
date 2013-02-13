4180db4bba338ab1cadb9ef66a5c456428cb57b0
Ralf Karrenberg <karrenberg@cdl.uni-saarland.de>
2013-02-13 14:38:33 +0100

    makenoise.sh now builds with -j2

--- makenoise.sh
+++ makenoise.sh
@@ -2,4 +2,4 @@ export LLVM_INSTALL_DIR=/home/karrenberg/proj/noise/install_debug
 export WFV_INSTALL_DIR=/home/karrenberg/proj/wfv2/build/linux-debug/
 
 # Build Clang.
-make clang-only -C ../../../build_debug/
+make clang-only -C ../../../build_debug/ -j2
